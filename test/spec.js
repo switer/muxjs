f92d762909e0f01ee922171045db1358acdafcc8
switer <guankaishe@gmail.com>
2015-01-17 20:18:49 +0800

    finished $and, $computed test case

--- test/spec.js
+++ test/spec.js
@@ -22,10 +22,12 @@ var Comment = Mux.extend({
 var comment = new Comment()
 
 test('props', function (t) {
-    t.plan(3)
+    t.plan(4)
     t.equal(comment.title, 'comment to me')
     comment.title = 'abc'
     t.equal(comment.title, 'abc')
+    comment['title'] = 123
+    t.equal(comment.title, 123)
     t.equal(comment.unobserved, undefined)
 })
 test('computed', function (t) {
@@ -80,14 +82,95 @@ test('$set && $watch && $unwatch', function (t) {
         })
         comment.$set('replyUsers', [1,2,3])
     })
-    t.test('Watch all properties', function (st) {
+    t.test('Watch any properties change', function (st) {
         st.plan(1)
         comment.$unwatch()
         comment.$watch(function () {
-            console.log('---------change')
             st.equal(this.replies, 2)
         })
         comment.$set('replyUsers', [1,2])
     })
+    t.test('Set multiple props', function (st) {
+        st.plan(3)
+        var count = 0
+        comment.$unwatch()
+        comment.$watch(function () {
+            st.equal(++count, 1)
+        })
+        var c2 = 0
+        comment.$watch(function () {
+            st.equal(++c2, 1)
+        })
+        comment.$set({
+            title: 'reset comment',
+            author: 'mux.js'
+        })
+        st.equal(comment.author, 'mux.js')
+    })
 })
-
+test('$add', function (t) {
+    t.test('observe a property', function (st) {
+        st.plan(2)
+        comment.$unwatch()
+        comment.$add('new')
+        comment.$watch('new', function () {
+            st.pass()
+        })
+        comment.$set('new', 'new property')
+        st.equal(comment.new, 'new property')
+    })
+    t.test('observe multiple properties', function (st) {
+        st.plan(4)
+        comment.$unwatch()
+        comment.$add('prop1', 'prop2')
+        comment.$watch('prop1', function (next, pre) {
+            st.equal(next, 'new property 1')
+        })
+        comment.$watch('prop2', function (next, pre) {
+            st.equal(next, 'new property 2')
+        })
+        comment.$set('prop1', 'new property 1')
+        comment.$set('prop2', 'new property 2')
+        st.equal(comment['prop1'], 'new property 1')
+        st.equal(comment['prop2'], 'new property 2')
+    })
+})
+test('$computed', function (t) {
+    t.test('Define a computed property', function (st) {
+        st.plan(1)
+        comment.$unwatch()
+        comment.$computed('computed1', ['title'], function () {
+            return 'Say:' + this.title
+        })
+        comment.$watch('computed1', function () {
+            st.equal(this.computed1, 'Say:hello')
+        })
+        comment.title = 'hello'
+    })
+    t.test('Define multiple computed properties', function (st) {
+        st.plan(3)
+        comment.$unwatch()
+        comment.$computed({
+            'computed2': {
+                deps:['title'], 
+                fn: function () {
+                    return 'Guankaishe say:' + this.title
+                }
+            },
+            'computed3': {
+                deps:['title'], 
+                fn: function () {
+                    return 'Switer say:' + this.title
+                }
+            }
+        })
+        comment.$watch('computed2', function () {
+            st.equal(this.computed2, 'Guankaishe say:world')
+        })
+        comment.$watch('computed3', function () {
+            st.equal(this.computed3, 'Switer say:world')
+        })
+        comment.title = 'world'
+        st.equal(comment.computed2, 'Guankaishe say:world')
+    })
+})
